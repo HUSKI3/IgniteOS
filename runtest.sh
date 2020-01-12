599e6f84c1dd0be1098259c430f986895ceb26a8
Linkus <31256742+HUSKI3@users.noreply.github.com>
2020-01-12 19:26:06 +0000

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -4,6 +4,15 @@
 set -ex
 
 cd fire_overlay
+echo "******************************"
+echo "***** glibc_full TEST BEGIN *****"
+echo "******************************"
+./overlay_clean.sh
+./overlay_build.sh glibc_full
+echo "****************************"
+echo "***** glibc_full TEST END *****"  
+echo "****************************"
+
 for bundle in `ls bundles` ; do
   echo "******************************"
   echo "***** $bundle TEST BEGIN *****"
