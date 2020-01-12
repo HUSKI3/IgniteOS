f8dc1024783201ac58b1fe9f736682cba02ece39
Linkus <31256742+HUSKI3@users.noreply.github.com>
2020-01-12 19:04:44 +0000

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,67 +1,17 @@
 #!/bin/sh
 
-set -e
 
-mkdir -p fire_overlay/rootfs/etc/autorun
-cat << CEOF > fire_overlay/rootfs/etc/autorun/99_autoshutdown.sh
-#!/bin/sh
+set -ex
 
-# This script shuts down the OS automatically.
-sleep 10 && poweroff &
-
-echo "  Ignite will shut down in 10 seconds."
-
-CEOF
-chmod +x fire_overlay/rootfs/etc/autorun/99_autoshutdown.sh
-
-cat <<CEOF > fire_boot/bios/boot/syslinux/syslinux.cfg
-SERIAL 0
-DEFAULT operatingsystem
-LABEL operatingsystem
-    LINUX /boot/kernel.xz
-    APPEND console=tty0 console=ttyS0
-    INITRD /boot/rootfs.xz
-
-CEOF
-
-./repackage.sh
-qemu-system-x86_64 -m 256M -cdrom IgniteOS.iso -boot d -localtime -nographic &
-
-sleep 5
-if [ "`ps -ef | grep -i [q]emu-system`" = "" ] ; then
-  echo "`date` | !!! FAILURE !!! Ignite is not running in QEMU."
-  exit 1
-else
-  echo "`date` | Ignite is running in QEMU. Waiting for automatic shutdown."
-fi
-
-RETRY=10
-while [ ! "$RETRY" = "0" ] ; do
-  echo "`date` | Countdown: $RETRY"
-  if [ "`ps -ef | grep -i [q]emu-system`" = "" ] ; then
-    break
-  fi
-  sleep 30
-  RETRY=$(($RETRY - 1))
+cd fire_overlay
+for bundle in `ls bundles` ; do
+  echo "******************************"
+  echo "***** $bundle TEST BEGIN *****"
+  echo "******************************"
+  ./overlay_clean.sh
+  ./overlay_build.sh $bundle
+  echo "****************************"
+  echo "***** $bundle TEST END *****"  
+  echo "****************************"
 done
 
-if [ "`ps -ef | grep -i [q]emu-system`" = "" ] ; then
-  echo "`date` | Ignite is not running in QEMU."
-else
-  echo "`date` | !!! FAILURE !!! Ignite is still running in QEMU."
-  exit 1
-fi
-
-cat << CEOF
-
-  ##################################################################
-  #                                                                #
-  # QEMU test passed. Clean manually the affected Ignite artifacts.#
-  #                                                                #
-  ##################################################################
-
-CEOF
-
-echo "`date` | *** Ignite QEMU test - END ***"
-
-set +e
